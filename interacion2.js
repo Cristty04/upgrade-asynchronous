4fe4514632e741443b210387a48645640f6ed62e
i-am-iron-man <iron-man@avengers.com>
2023-01-21 21:07:56 +0100

    last commit

--- interacion2.js
+++ interacion2.js
@@ -1,12 +1,13 @@
 //2.1 Convierte la siguiente promesa para esperar a ejecutar el console usando async-await.
-const runTimeOut = () => {
-    const promise = new Promise((resolve) => {
+const runTimeOut = async () => {
+    const promise = new Promise((resolve) =>  {
         setTimeout(function () {
             resolve();
         }, 2000);
     })
+        await promise 
 
-    promise.then(() => {console.log('Time out!')})
+   console.log('Time out!')
 };
 
 runTimeOut();
@@ -14,9 +15,10 @@ runTimeOut();
 //2.2 Convierte la siguiente función con un fetch utilizando async-await.Recuerda que para usar .fetch() 
 //tendrás que probar el ejercicio en el navegador; 
 
-function getCharacters () {
-    fetch('https://rickandmortyapi.com/api/character').then(res => res.json()).then(characters => 
-    console.log(characters));
+const getCharacters = async () => {
+    const variable = await fetch('https://rickandmortyapi.com/api/character')
+    const res = await variable.json()
+    console.log(res);
 }
 
 getCharacters();
